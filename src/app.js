b003da9f5b803d3b4772458151ce43fdb84ae09b
carmenllenado <39427352+carmenllenado@users.noreply.github.com>
2023-08-26 15:28:32 +0800

    Forecast and date/time

--- src/app.js
+++ src/app.js
@@ -1,18 +1,43 @@
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday"
+    ];
+    let day = days[date.getDay()];
+    return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
-    console.log(response.data);
     let temperatureDisplay = document.querySelector("#temperature");
     let cityDisplay = document.querySelector("#city");
     let descriptionDisplay = document.querySelector("#description");
     let humidityDisplay = document.querySelector("#humidity");
     let windDisplay = document.querySelector("#wind");
+    let dateDisplay = document.querySelector("#date");
     temperatureDisplay.innerHTML = Math.round(response.data.temperature.current);
     cityDisplay.innerHTML = response.data.city;
     descriptionDisplay.innerHTML = response.data.condition.description;
     humidityDisplay.innerHTML = response.data.temperature.humidity;
     windDisplay.innerHTML = Math.round(response.data.wind.speed);
+    dateDisplay.innerHTML = formatDate(response.data.time * 1000);
 }
 
-let city = `Makati`;
+let city = `Santa Barbara`;
 let apiKey = `faca83b09f0bt8700a3e54o84043fbae`;
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
